511658aaa8aa18f340c398eac262aedab6a5511f
Matt Schertzer <matthew.schertzer@gmail.com>
2025-04-12 00:12:58 -0400

    Example code we worked on

--- FrontEnd/website/src/app/app.component.ts
+++ FrontEnd/website/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
